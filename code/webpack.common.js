c094e25f42b6c14cd5f651e7211cc531740a0140
WutBottle <785012476@qq.com>
2020-06-30 19:32:49 +0800

    修复了关于webpack-dev-server的打包错误

---
+++ code/webpack.common.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, './src/main.js'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [{
+      test: /.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      },
+    }, {
+      test: /\.(png|gif|jpg|svg|jpeg)$/i,
+      loader: 'url-loader',
+      options: {
+        limit: 2048,
+        name: 'assets/[name].[ext]',
+        esModule: false,
+      },
+    }, {
+      test: /\.(less|css)$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'less-loader',
+      ]
+    }, {
+      test: /.vue$/,
+      loader: 'vue-loader'
+    }],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      template: 'public/index.html',
+      title: 'vue-cli',
+      favicon: 'public/favicon.ico',
+      inject: true,
+    }),
+  ]
+};
